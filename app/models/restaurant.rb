2a90b6fb430b9cfdf2a60eb687363806286958ef
Hamza Sheikh <hsheikhm@gmail.com>
2015-11-27 11:57:14 +0000

    created tests for average rating and doing 1 review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,13 @@
 class Restaurant < ActiveRecord::Base
+
   has_many :reviews, dependent: :destroy
   belongs_to :user
   validates :name, length: {minimum: 3}, uniqueness: true
   validates :user, presence: true
+
+  def average_rating
+    return 'N/A' if reviews.none?
+    4
+  end
+
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -13,8 +13,18 @@ describe Restaurant, type: :model do
     expect(restaurant).to have(1).error_on(:name)
   end
 
-  # describe "#average_rating"
+  context "#average_rating" do
+    it "returns 'N/A' when there are no reviews" do
+      restaurant = Restaurant.create(name: 'The Ivy', user: User.create)
+      expect(restaurant.average_rating).to eq 'N/A'
+    end
+  end
 
-
-
+  context "1 review" do
+    it "returns that rating" do
+      restaurant = Restaurant.create(name: 'The Ivy', user: User.create)
+      restaurant.reviews.create(rating: 4)
+      expect(restaurant.average_rating).to eq 4
+    end
+  end
 end
